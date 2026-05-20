dc7b7f868788976bdcfa9a764463f9327a27214a
Erick_Mazur <mazur.negocios@gmail.com>
2026-05-20 10:29:17 -0300

    profile user concluido

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -20,3 +21,4 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user}'
+    
